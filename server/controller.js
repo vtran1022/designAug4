4a6a7763782685a7cda337613bebf5cd636dea7d
Vy Tran <vtran1022@Gmail.com>
2021-08-05 13:34:25 -0700

    set up controller

---
+++ server/controller.js
@@ -0,0 +1,24 @@
+const { fetchDogs, addDog } = require('../db/queries.js');
+
+const getDogs = (req, res) => {
+  fetchDogs()
+    .then((data) => res.status(200).send(data))
+    .catch((err) => {
+      res.status(500).send(`Error fetching dogs: ${err}`);
+      console.error(`Error fetching dogs: ${err}\n\n${err.stack}`);
+    });
+};
+
+const postDog = (req, res) => {
+  addDog(req.body.name, req.body.breed)
+    .then((response) => res.status(200).send(`Successfully added dog! System message:${response}`))
+    .catch((err) => {
+      res.status(500).send(`Error adding dog: ${err}`);
+      console.error(`Error adding dog: ${err}\n\n${err.stack}`);
+    });
+};
+
+module.exports = {
+  getDogs,
+  postDog
+};
