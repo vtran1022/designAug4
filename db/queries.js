4bb0a540ff76d3d6782031e588c30efa8c45309d
Vy Tran <vtran1022@Gmail.com>
2021-08-05 13:28:21 -0700

    set up basic fetch and add queries

---
+++ db/queries.js
@@ -0,0 +1,16 @@
+const pool = require('./connection.js');
+
+const fetchDogs = () => {
+  const queryStr = 'SELECT * FROM dogs';
+  return pool.query(queryStr).then((data) => data.rows);
+};
+
+const addDog = (name, breed) => {
+  const queryStr = 'INSERT INTO dogs (name, breed) VALUES ($1, $2)';
+  return pool.query(queryStr).then((response) => response);
+};
+
+module.exports = {
+  fetchDogs,
+  addDog
+};
