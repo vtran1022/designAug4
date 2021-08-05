94d6b0628ceada7425788c520476f046cf8465da
Vy Tran <vtran1022@Gmail.com>
2021-08-05 09:33:27 -0700

    server file set

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config(); // used to read .env file
+
+const express = require('express');
+const router = require('./routers.js');
+const morgan = require('morgan'); // middleware to track traffic in development
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(process.env.PORT, function () {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
