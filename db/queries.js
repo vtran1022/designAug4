f57e638c055b7454af0970454b2e1246c2bbd932
Vy Tran <vtran1022@Gmail.com>
2021-08-08 19:39:34 -0700

    updated two queries

--- db/queries.js
+++ db/queries.js
@@ -1,12 +1,19 @@
 const pool = require('./connection.js');
 
 const fetchDogs = () => {
-  const queryStr = 'SELECT * FROM dogs';
+  const queryStr = `SELECT dog.name, breed.breed
+                    FROM (SELECT name, breed_id FROM dogs) AS dog
+                    JOIN (SELECT breed, breed_id FROM breeds) AS breed
+                    ON dog.breed_id = breed.breed_id`;
   return pool.query(queryStr).then((data) => data.rows);
 };
 
 const fetchADog = (name) => {
-  const queryStr = 'SELECT * FROM dogs WHERE name = $1';
+  const queryStr = `SELECT dog.name, breed.breed
+                    FROM (SELECT name, breed_id FROM dogs) AS dog
+                    JOIN (SELECT breed, breed_id FROM breeds) AS breed
+                    ON dog.breed_id = breed.breed_id
+                    WHERE dog.name = $1`;
   return pool.query(queryStr, [name]).then((data) => {
     if (data.rows.length === 0) {
       return `Dog with the name ${name} does not exist`;
