77a5ef0ec913f1c1908f705393e87f8cc1bc888f
Vy Tran <vtran1022@Gmail.com>
2021-08-08 18:58:25 -0700

    splitting table into two tables, added comments

--- schema.sql
+++ schema.sql
@@ -1,5 +1,19 @@
-CREATE TABLE IF NOT EXISTS dogs (
-  dogs_id SERIAL PRIMARY KEY,
-  name VARCHAR(200) NOT NULL,
+CREATE TABLE IF NOT EXISTS breeds (
+  breed_id SERIAL PRIMARY KEY,
   breed VARCHAR(200) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS dogs (
+  dog_id SERIAL PRIMARY KEY,
+  name VARCHAR(200) NOT NULL,
+  CONSTRAINT fk_breeds
+  FOREIGN KEY(breed_id)
+    REFERENCES breeds(id)
+);
+
+/* run docker
+docker run --rm --name testdb -e POSTGRES_PASSWORD=<password here> -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
+
+login
+psql -h localhost -U test_user -d test_db
+*/
